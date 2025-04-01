ac6ac419cbf3dd1409dd92aab55682ce4ff3d41d
brucantacini <brunocantacini.ti@gmail.com>
2025-04-01 11:05:53 -0300

    Coletando livro por ID.

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { getLivros } = require('../controladores/livros');
+const { getLivros, getLivro } = require('../controladores/livros');
 const router = Router();
 
 router.get('/', getLivros);
+
+router.get('/:id', getLivro);
 
 router.post('/', (req, res) => {
     res.send("Você fez uma requisição POST")
