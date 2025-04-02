7fb6cae25a05dfb23a8eb72b89699d0c7c6ec04d
brucantacini <brunocantacini.ti@gmail.com>
2025-04-02 14:39:53 -0300

    Testando conexao app

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const rotaLivro = require('./rotas/livro');
-const cors = require('cors');
+const express = require("express")
+const rotaLivro = require("./rotas/livro")
+const cors = require("cors")
 
-const app = express();
-app.use(express.json());
-app.use(cors( {origin: "*"} ));
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "*"}))
 
-app.use('/livros', rotaLivro);
+app.use('/livros', rotaLivro)
 
-const port = 8000;
-
+const port = 8000
 
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`);
+    console.log(`Escutando a porta ${port}`)
 })
